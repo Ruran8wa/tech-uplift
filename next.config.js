6f0bf21cfd900fe494594a8538cd1f819cf5523f
ruran8wa <p.rurangwa@alustudent.com>
2024-11-25 15:49:03 +0200

    final xx

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        runtime: 'nodejs',
+      },
     images: {
         domains: ["utfs.io"] // Allow all domains
     }
